7989fe3832024bccc8d254845f3d8fd738e7ee7a
Elie Pichon <elie.pichon@chabe.fr>
2020-03-18 15:18:40 +0000

    Pull all test vehicle in single file to use same objects

--- test/vehicles.test.js
+++ test/vehicles.test.js
@@ -1,12 +1,71 @@
-const assert =require('assert');
+const assert = require('assert');
 const GWCaller = require('../services/GWCaller')
-
 const gwCaller = new GWCaller();
 
+
+
+// MODELS
+const testModel = {
+  name: 'resmodel1',
+  make: 'MERCEDES',
+  category: 'B',
+};
+const testModelUpper = Object.assign({}, testModel);
+Object.keys(testModelUpper).forEach(k => testModelUpper[k] = testModelUpper[k].toUpperCase());
+
+describe('MOUNT MODELS API', () => {
+  const testUpdate = {
+    category: 'D',
+    make: 'RENAULT',
+  };
+
+  describe('POST', () => {
+    it('should return a synchronized models', async () => {
+      const postModel = await gwCaller.call(
+        'POST', '/vehicles/models/', {}, {}, testModel,
+      );  
+      assert.equal(postModel.data.message, 'Model synchronised');
+      assert.deepEqual(postModel.data.model, testModelUpper);
+    });
+  });
+
+  describe('GET', () => {
+    it('should return the model', async () => {
+      const getModel = await gwCaller.call(
+        'GET', `/vehicles/models/${testModel.name}`, {}, {}, {},
+      );  
+      assert.deepEqual(getModel.data, testModelUpper);
+    });
+  });
+
+  describe('PATCH', () => {
+    it('should update the model', async () => {
+      const patchModel = await gwCaller.call(
+        'PATCH', `/vehicles/models/${testModel.name}`, {}, {}, testUpdate,
+      );  
+      assert.equal(patchModel.data.message, `Model ${testModelUpper.name} update success`);
+      assert.deepEqual(patchModel.data.model, testUpdate);
+    });
+  });
+
+  describe('GET', () => {
+    it('should return the updated model', async () => {
+      const getModel = await gwCaller.call(
+        'GET', `/vehicles/models/${testModel.name}`, {}, {}, {},
+      );
+      Object.keys(testUpdate).forEach(key => {
+        assert.equal(getModel.data[key], testUpdate[key]);
+      });
+    });
+  });
+});
+
+// VEHICLES
 const testVehi = {
-  vehicleId: "Reserved1",
-  registration: "RESERVED1",
+  vehicleId: "Reserved2",
+  registration: "RESERVED2",
   label: "labeL1",
+  modelName: testModel.name,
   mileage: 666,
   nbSeats: 0,
   firstRegDate: "2015-01-01",
@@ -16,9 +75,8 @@ const testVehi = {
     agency: "CHABE",
 }]};
 
-describe('Test VEHICLES API', () => {
+describe('MOUNT VEHICLES API', () => {
 
-  let theModel;
   const testUpdate = {
     label: "label2",
     mileage: 777,
@@ -27,37 +85,12 @@ describe('Test VEHICLES API', () => {
   };
   const vehiId = testVehi.vehicleId.toUpperCase();
 
-  describe('GET ALL', () => {
-    it('should return models to pick the first', async () => {
-      const allModels = await gwCaller.call(
-        'GET', `/vehicles/models/`, {}, {}, {},
-      );
-      theModel = allModels.data[0];
-      testVehi.modelName = theModel.name;
-      assert.ok(testVehi.modelName);
-      assert.ok(theModel);
-    });
-  });
-
-  describe('DELETE', () => {
-    it('should delete model or the ID is free', async () => {
-      try {
-        const delVehi = await gwCaller.call(
-          'DELETE', `/vehicles/${testVehi.vehicleId}`, {}, {}, {},
-        );
-        assert.equal(delVehi.data.message, 'Vehicle deleted');
-      } catch (e) {
-        assert.equal(`Vehicle ${vehiId} not found`, e.data.error);
-      }
-    });
-  });
-
   describe('POST', () => {
     it('should synchronize vehicle', async () => {
       const postVehi = await gwCaller.call(
         'POST', '/vehicles/', {}, {}, testVehi,
       );  
-      assert.deepEqual(postVehi.data.vehicleId, vehiId);
+      assert.equal(postVehi.data.vehicleId, vehiId);
     });
   });
 
@@ -73,7 +106,7 @@ describe('Test VEHICLES API', () => {
         mileage: testVehi.mileage,
         firstRegDate: "2015-01-01",
         nbSeats: testVehi.nbSeats,
-        modelName: testVehi.modelName,
+        modelName: testVehi.modelName.toUpperCase(),
       });
     });
   });
@@ -103,4 +136,134 @@ describe('Test VEHICLES API', () => {
   });
 });
 
-module.exports = testVehi;
+
+// UNAVAILABILITIES
+const testUnav = {
+  idUnavailability: "RES2",
+  type: "REPARATION",
+  startDate: "2020-03-15 14:00:00",
+  endDate: "2020-03-16 18:30:00",
+  informations: "truc truc"
+};
+
+describe('MOUNT unavailabilities API', () => {
+
+  const testUpdate = {
+    informations: "truc2",
+    endDate: "2020-03-16 10:30:00",
+  };
+  const vehicleId = 'RESERVED1';
+
+  describe('POST', () => {
+    it('should post unav', async () => {
+      const postUnav = await gwCaller.call(
+        'POST', `/vehicles/${vehicleId}/unavailabilities/`, {}, {}, testUnav,
+      );  
+      assert.deepEqual(postUnav.data.unavailability, testUnav);
+    });
+  });
+
+  describe('GET MANY UNAV', () => {
+    it('should return the unav', async () => {
+      const getManyUnav = await gwCaller.call(
+        'GET', `/vehicles/${vehicleId}/unavailabilities/`,
+        { 
+          from: "2019-01-01 14:00:00",
+          to: "2020-12-30 18:30:00",
+        } , {}, {},
+      );
+      assert(
+        getManyUnav.data.find(unav => unav.idUnavailability === testUnav.idUnavailability) !== undefined,
+        'Unavailability succesffully removed',
+      );
+    });
+  });
+  
+  describe('GET', () => {
+    it('should get the unav', async () => {
+      const postUnav = await gwCaller.call(
+        'GET', `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`, {}, {}, {},
+      );  
+      assert.deepEqual(postUnav.data, {
+        idUnavailability: testUnav.idUnavailability.toUpperCase(),
+        vehicleId: vehicleId.toUpperCase(),
+        type: testUnav.type.toUpperCase(),
+        startDate: testUnav.startDate,
+        endDate: testUnav.endDate,
+        informations: testUnav.informations.toUpperCase(),
+      });
+    });
+  });
+
+  describe('PATCH', () => {
+    it('should update the unav', async () => {
+      const patchUnav = await gwCaller.call(
+        'PATCH', `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`, {}, {}, testUpdate,
+      );  
+      assert.equal(patchUnav.data.message, 'Unavailability succesffully updated');
+    });
+  });
+
+  describe('GET', () => {
+    it('should return the updated unav', async () => {
+      const getUnav = await gwCaller.call(
+        'GET', `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`, {}, {}, {},
+      );
+      Object.keys(testUpdate).forEach(key => {
+        if(typeof testUpdate[key] === 'string') {
+          assert.equal(getUnav.data[key], testUpdate[key].toUpperCase());
+        } else {
+          assert.equal(getUnav.data[key], testUpdate[key]);
+        }
+      });
+    });
+  });
+
+  describe('DELETE', () => {
+    it('should delete unav', async () => {
+      const delVehi = await gwCaller.call(
+        'DELETE', `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`, {}, {}, {},
+      );
+      assert.equal(delVehi.data.message, 'Unavailability succesffully removed');
+    });
+  });
+
+});
+
+
+describe('UNMOUNT VEHICLES', () => {
+  describe('DELETE', () => {
+    it('should delete model or the ID is free', async () => {
+      try {
+        const delVehi = await gwCaller.call(
+          'DELETE', `/vehicles/${testVehi.vehicleId}`, {}, {}, {},
+        );
+        assert.equal(delVehi.data.message, 'Vehicle deleted');
+      } catch (e) {
+        assert.equal(`Vehicle ${testVehi.vehicleId.toUpperCase()} not found`, e.data.error);
+      }
+    });
+  });
+});
+
+describe('UNMOUNT MODELS', () => {
+  describe('DELETE', () => {
+    it('should delete model', async () => {
+      const delModel = await gwCaller.call(
+        'DELETE', `/vehicles/models/${testModel.name}`, {}, {}, {},
+      );
+      assert.equal(delModel.data.message, `Model ${testModelUpper.name} DELETE success`);
+    });
+  });
+  
+  describe('GET ALL', () => {
+    it('should return models, not the deleted one', async () => {
+      const allModels = await gwCaller.call(
+        'GET', `/vehicles/models/`, {}, {}, {},
+      );
+      assert.equal(allModels.data.find(model => model.name === testModel.name), undefined);
+    });
+  });  
+});
+
+
