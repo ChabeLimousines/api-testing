a3705a362f56c893dc41f220a79b37a42548959f
Elie Pichon <elie.pichon@chabe.fr>
2020-03-30 18:09:21 +0100

    fix tests

--- test/vehicles.test.js
+++ test/vehicles.test.js
@@ -182,19 +182,18 @@ describe('MOUNT unavailabilities API', () => {
     informations: 'truc2',
     endDate: '2020-03-16 10:30:00',
   };
-  const vehicleId = 'RESERVED1';
 
   describe('test void unav', () => {
     it('get should return a 404', async () => {
-      const getVoid = await tryCall('GET', `/vehicles/${vehicleId}/unavailabilities/NOPE`);
+      const getVoid = await tryCall('GET', `/vehicles/${testVehi.vehicleId}/unavailabilities/NOPE`);
       assert.equal(getVoid.status, 404);
     });
     it('delete should return a 404', async () => {
-      const getVoid = await tryCall('DELETE', `/vehicles/${vehicleId}/unavailabilities/NOPE`);
+      const getVoid = await tryCall('DELETE', `/vehicles/${testVehi.vehicleId}/unavailabilities/NOPE`);
       assert.equal(getVoid.status, 404);
     });
     it('patch should return a 404', async () => {
-      const getVoid = await tryCall('PATCH', `/vehicles/${vehicleId}/unavailabilities/NOPE`);
+      const getVoid = await tryCall('PATCH', `/vehicles/${testVehi.vehicleId}/unavailabilities/NOPE`);
       assert.equal(getVoid.status, 404);
     });
   });
@@ -202,12 +201,12 @@ describe('MOUNT unavailabilities API', () => {
 
   describe('POST', () => {
     it('should post unav', async () => {
-      const postUnav = await tryCall('POST', `/vehicles/${vehicleId}/unavailabilities/`, testUnav);
+      const postUnav = await tryCall('POST', `/vehicles/${testVehi.vehicleId}/unavailabilities/`, testUnav);
       assert.deepEqual(postUnav.data.unavailability, testUnav);
     });
-    it('should post unav', async () => {
-      const postUnav = await tryCall('POST', `/vehicles/${vehicleId}/unavailabilities/`, testUnav);
-      assert.deepEqual(postUnav.data.unavailability, testUnav);
+    it('should return 409 on second call', async () => {
+      const postUnav = await tryCall('POST', `/vehicles/${testVehi.vehicleId}/unavailabilities/`, testUnav);
+      assert.deepEqual(postUnav.status, 409);
     });
   });
 
@@ -229,7 +228,7 @@ describe('MOUNT unavailabilities API', () => {
 
   describe('GET MANY UNAV', () => {
     it('should return the unav', async () => {
-      const getManyUnav = await tryCall('GET', `/vehicles/${vehicleId}/unavailabilities/`, {},
+      const getManyUnav = await tryCall('GET', `/vehicles/${testVehi.vehicleId}/unavailabilities/`, {},
         {
           from: '2019-01-01 14:00:00',
           to: '2020-12-30 18:30:00',
@@ -245,10 +244,10 @@ describe('MOUNT unavailabilities API', () => {
   describe('GET', () => {
     it('should get the unav', async () => {
       const getUnav = await tryCall('GET',
-        `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`);
+        `/vehicles/${testVehi.vehicleId}/unavailabilities/${testUnav.idUnavailability}`);
       assert.deepEqual(getUnav.data, {
         idUnavailability: testUnav.idUnavailability.toUpperCase(),
-        vehicleId: vehicleId.toUpperCase(),
+        vehicleId: testVehi.vehicleId.toUpperCase(),
         type: testUnav.type.toUpperCase(),
         startDate: testUnav.startDate,
         endDate: testUnav.endDate,
@@ -260,7 +259,7 @@ describe('MOUNT unavailabilities API', () => {
   describe('PATCH', () => {
     it('should update the unav', async () => {
       const patchUnav = await tryCall('PATCH',
-        `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`, testUpdate);
+        `/vehicles/${testVehi.vehicleId}/unavailabilities/${testUnav.idUnavailability}`, testUpdate);
       assert.equal(patchUnav.data.message, 'Unavailability succesffully updated');
     });
   });
@@ -268,9 +267,15 @@ describe('MOUNT unavailabilities API', () => {
   describe('GET', () => {
     it('should return the updated unav', async () => {
       const getUnav = await tryCall('GET',
-        `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`);
-      Object.keys(testUpdate).forEach((key) => {
-        assert.equal(getUnav.data[key], testUpdate[key]);
+        `/vehicles/${testVehi.vehicleId}/unavailabilities/${testUnav.idUnavailability}`);
+      assert.equal(getUnav.status, 200);
+      assert.deepEqual(getUnav.data, {
+        idUnavailability: testUnav.idUnavailability.toUpperCase(),
+        vehicleId: testVehi.vehicleId.toUpperCase(),
+        type: testUnav.type.toUpperCase(),
+        startDate: testUnav.startDate,
+        endDate: testUpdate.endDate,
+        informations: testUpdate.informations,
       });
     });
   });
@@ -278,15 +283,14 @@ describe('MOUNT unavailabilities API', () => {
   describe('DELETE', () => {
     it('should delete unav', async () => {
       const delVehi = await tryCall('DELETE',
-        `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`);
+        `/vehicles/${testVehi.vehicleId}/unavailabilities/${testUnav.idUnavailability}`);
       assert.equal(delVehi.data.message, 'Unavailability succesffully removed');
     });
   });
 
   describe('GET', () => {
     it('should return an 404', async () => {
-      const getDeleted = await gwCaller
-        .call('GET', `/vehicles/${vehicleId}/unavailabilities/${testUnav.idUnavailability}`);
+      const getDeleted = await tryCall('GET', `/vehicles/${testVehi.vehicleId}/unavailabilities/${testUnav.idUnavailability}`);
       assert.equal(getDeleted.status, 404);
     });
   });
