9797ce21f148b3267abc86858c1abbae9484a6f2
EPichon <elie.pichon@chabe.fr>
2020-06-03 11:44:16 +0100

    Pass basic test for 400 and 404

--- test/requests/request.test.js
+++ test/requests/request.test.js
@@ -12,7 +12,8 @@ const placeFull = { placeId: 'CDG' };
 const placeMin = { placeId: 'CDG' };
 const passFull = { passengerId: 928565 };
 const passMin = { passengerId: 928565 };
-
+const client = 'PDG';
+const voidRequestId = '1234567890abcdefABCDEF1f';
 const requestPostIn = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'request.post.in.json'), 'utf8'));
 console.log(requestPostIn);
 // requestPostIn.dropOffPlace.placeId = placeFull.placeId;
@@ -23,48 +24,60 @@ requestPostIn.request[0].passenger.passengerId = passMin;
 
 function testMountRequest() {
   describe('test bad requests data', () => {
+    it('should return a 400 incorrect id', async () => {
+      const getVoid = await tryCall('GET', '/requests/0', {}, { client });
+      assert.equal(getVoid.status, 400);
+    });
+    it('should return a 400 incorrect id', async () => {
+      const getVoid = await tryCall('POST', '/requests/0/modify', {}, { client });
+      assert.equal(getVoid.status, 400);
+    });
+    it('should return a 400 incorrect id', async () => {
+      const getVoid = await tryCall('POST', '/requests/0/cancel', {}, { client });
+      assert.equal(getVoid.status, 400);
+    });
     it('GET should return a 404', async () => {
-      const getVoid = await tryCall('GET', '/requests/0');
+      const getVoid = await tryCall('GET', `/requests/${voidRequestId}`, {}, { client });
       assert.equal(getVoid.status, 404);
     });
     it('POST update should return a 404', async () => {
-      const getVoid = await tryCall('POST', '/requests/0/update');
+      const getVoid = await tryCall('POST', `/requests/${voidRequestId}/modify`, {}, { client });
       assert.equal(getVoid.status, 404);
     });
     it('POST cancel should return a 404', async () => {
-      const getVoid = await tryCall('POST', '/requests/0/cancel');
+      const getVoid = await tryCall('POST', `/requests/${voidRequestId}/cancel`, {}, { client });
       assert.equal(getVoid.status, 404);
     });
   });
 
-  describe('Creation new request', () => {
-    it('Should create a new request', async () => {
-      const getVoid = await tryCall('POST', '/requests');
-      assert.equal(getVoid.status, 201);
-      // todo assert return message ok
-    });
-    it('Should get this request by RequestId', async () => {
-      const getVoid = await tryCall('GET', `/requests/${requestPostIn.requestId}`);
-      assert.equal(getVoid.status, 200);
-      // todo assert deepequal
-    });
-    it('Should get this request by OB id', async () => {
-      const getVoid = await tryCall('GET', `/requests/byOBid/${requestPostIn.tempOBid}`);
-      assert.equal(getVoid.status, 200);
-    });
-    it('Should find this request when get by client', async () => {
-      const getMany = await tryCall('GET', '/requests/byClients/');
-      assert.equal(getMany.status, 200);
-      assert.include(getMany, { _id: requestPostIn.requestId });
-    });
-  });
+  // describe('Creation new request', () => {
+  //   it('Should create a new request', async () => {
+  //     const getVoid = await tryCall('POST', '/requests');
+  //     assert.equal(getVoid.status, 201);
+  //     // todo assert return message ok
+  //   });
+  //   it('Should get this request by RequestId', async () => {
+  //     const getVoid = await tryCall('GET', `/requests/${requestPostIn.requestId}`);
+  //     assert.equal(getVoid.status, 200);
+  //     // todo assert deepequal
+  //   });
+  //   it('Should get this request by OB id', async () => {
+  //     const getVoid = await tryCall('GET', `/requests/byOBid/${requestPostIn.tempOBid}`);
+  //     assert.equal(getVoid.status, 200);
+  //   });
+  //   it('Should find this request when get by client', async () => {
+  //     const getMany = await tryCall('GET', '/requests/byClients/');
+  //     assert.equal(getMany.status, 200);
+  //     assert.include(getMany, { _id: requestPostIn.requestId });
+  //   });
+  // });
 
-  describe('Incorrect uses of good request', () => {
-    it('GET without a client should 400', async () => {
-      const getVoid = await tryCall('GET', `/requests/${requestPostIn.idRequest}`);
-      assert.equal(getVoid.status, 400);
-    });
-  });
+  // describe('Incorrect uses of good request', () => {
+  //   it('GET without a client should 400', async () => {
+  //     const getVoid = await tryCall('GET', `/requests/${requestPostIn.idRequest}`);
+  //     assert.equal(getVoid.status, 400);
+  //   });
+  // });
 }
 
 function testUnmountRequest() {
